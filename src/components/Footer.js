337d0531f44b3fbdbf11b7200673b6eeadf60397
brynnke <100329799+brynnke@users.noreply.github.com>
2022-07-22 13:10:46 -0500

    added descption to footer and finished return function

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="content has-text-centered">
+                <p>
+                    <strong> Full Stack Development Portfolio</strong> - a React side by{""}
+                    <a href="https://github.com/brynnke" target="_blank" rel = "noreferrer">
+                        Brynn Kiloran
+                    </a>
+                    .
+                    <hr />
+                    <a
+                    href="comming soon"
+                    target="_blank" rel="noreferrer"
+                    >
+                        LinkedIn
+                    </a>{" "}
+                </p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
